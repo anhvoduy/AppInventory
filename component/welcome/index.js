a395787314260ff307336fc6516580f1c834d966
devinsg <duyanh2005@gmail.com>
2018-09-01 23:20:40 +0700

    clean up

--- component/welcome/index.js
+++ component/welcome/index.js
@@ -1,28 +1,22 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { View, Text } from 'react-native';
+import React from 'react';
+import { Platform, View, Text } from 'react-native';
 import styles from '../../Styles';
 
-class Welcome extends Component {
-    constructor(props) {
-        super(props);
-    }
+const instructions = Platform.select({
+    ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+    android:
+      'Double tap R on your keyboard to reload,\n' +
+      'Shake or press menu button for dev menu',
+});
 
-    render() {
-        let { authInfo, onLogout } = this.props;
-        return (
-            <View style={styles.container}>
-                <Text>Welcome { authInfo.user ? authInfo.user.name : ''}</Text>
-                <Text onPress={() => onLogout()}>Logout</Text>
-            </View>
-        );
-    }
-};
-
-Welcome.propTypes = {
-    title: PropTypes.string,
-    authInfo: PropTypes.object,
-    onLogout: PropTypes.func
+const Welcome = () => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.welcome}>Welcome to React Native!</Text>
+            <Text style={styles.instructions}>To get started, edit App.js</Text>
+            <Text style={styles.instructions}>{instructions}</Text>
+        </View>
+    );
 };
 
 export default Welcome;
