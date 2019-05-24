6a6a4d193377193be8a7ebca2db61fc2f7e362af
devinsg <duyanh2005@gmail.com>
2019-05-24 11:02:42 +0000

    make loading

--- component/productList/index.js
+++ component/productList/index.js
@@ -1,23 +1,35 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { View, Text } from 'react-native';
+import { View, Text, ActivityIndicator } from 'react-native';
 
 class ProductList extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            showProgress: false
+            showProgress: true
         }
     }
 
     componentDidMount() {
-        let { title, searchQuery } = this.props;
-        console.log('- title:', title);
+        let { searchQuery } = this.props;
         console.log('- searchQuery:', searchQuery);
+
+        let self = this;
+        setTimeout(function() {
+            self.setState({ showProgress: false });
+        }, 3000);
     }
 
     render(){
+        if(this.state.showProgress){
+            return (
+                <View style={{ flex: 1, justifyContent: 'center' }}>
+                    <ActivityIndicator size='large' animating={true} />
+                </View>
+            )
+        }
+
         return (
             <View style={{ flex: 1, justifyContent: 'flex-start' , paddingTop: 60 }}>
                 <Text>Display Product ListView</Text>
